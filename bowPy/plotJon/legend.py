57e2d17e84fae1b1a8e24aceb91378a07aaeddbf
jonbowr <jonbowr@yahoo.com>
2022-12-14 12:55:26 -0500

    added above option to legend_loc, fixed location below

--- bowPy/plotJon/legend.py
+++ bowPy/plotJon/legend.py
@@ -2,30 +2,44 @@ from matplotlib import pyplot as plt
 from matplotlib import container
 
 
-def legend_loc(fig,ax,label = '',location = 'rightside_outside',size = '10%',pad = .05,loc = 'upper left'):
-
-    leg = ax.legend()
-    figsize = fig.get_size_inches()
-
-    if location == 'right_outside':
-        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).width/fig.get_window_extent().width
-        leg.remove()
-        fig.set_figwidth(figsize[0]*(1+w))
-        fig.subplots_adjust(right=1-w)
-        cax = fig.add_axes([1-w, 0, 1, .98])
-    elif location =='below':
-        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height
-        leg.remove()
-        fig.set_figheight(figsize[1]*(1+w))
-        fig.subplots_adjust(bottom=w)
-        cax = fig.add_axes([0, 0, 1, w])
-
-    cax.set_axis_off()
+def legend_loc(fig,ax,label = '',location = 'right',size = '10%',pad = .05):
 
     # get handles
     handles, labels = ax.get_legend_handles_labels()
     # remove the errorbars
     handles = [h[0] if isinstance(h, container.ErrorbarContainer) else h for h in handles]
 
+    leg = ax.legend(handles = handles,labels = labels,title = label)
+
+    figsize = fig.get_size_inches()
+
+    if location == 'right':
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).width/fig.get_window_extent().width
+        leg.remove()
+        fig.set_figwidth(figsize[0]*(1+w))
+
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).width/fig.get_window_extent().width
+        fig.subplots_adjust(right=1-w)
+        cax = fig.add_axes([1-w, 0, 1, .98])
+        loc = 'upper left'
+    elif location =='below':
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height
+        fig.set_figheight(figsize[1]*(1+w))
+
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height
+        fig.subplots_adjust(bottom=w+fig.subplotpars.hspace)
+        cax = fig.add_axes([0, 0, 1, w])
+        loc = 'lower left'
+    elif location =='above':
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height
+        fig.set_figheight(figsize[1]*(1+w))
+
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height
+        fig.subplots_adjust(top=1-w)
+        cax = fig.add_axes([0, 1-w, 1, 1])
+        loc = 'lower left'
+    leg.remove()
+
+    cax.set_axis_off()
+
     cax.legend(loc = loc,handles = handles,labels = labels,title = label)
-    # fig.add_subplot(cax)
