8883ba69ca3f519693485503f49ac2cea3cadbe9
jonbowr <jonbowr@yahoo.com>
2024-08-16 09:33:30 -0400

    added pad to legend

--- bowPy/plotJon/legend.py
+++ bowPy/plotJon/legend.py
@@ -14,27 +14,27 @@ def legend_loc(fig,ax,label = '',location = 'right',size = '10%',pad = .05):
     figsize = fig.get_size_inches()
 
     if location == 'right':
-        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).width/fig.get_window_extent().width
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).width/fig.get_window_extent().width*(1+pad)
         leg.remove()
         fig.set_figwidth(figsize[0]*(1+w))
 
-        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).width/fig.get_window_extent().width
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).width/fig.get_window_extent().width*(1+pad)
         fig.subplots_adjust(right=1-w)
         cax = fig.add_axes([1-w, 0, 1, .98])
         loc = 'upper left'
     elif location =='below':
-        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height*(1+pad)
         fig.set_figheight(figsize[1]*(1+w))
 
-        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height*(1+pad)
         fig.subplots_adjust(bottom=w+fig.subplotpars.hspace)
         cax = fig.add_axes([0, 0, 1, w])
         loc = 'lower left'
     elif location =='above':
-        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height*(1+pad)
         fig.set_figheight(figsize[1]*(1+w))
 
-        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height
+        w = leg.get_window_extent(renderer = fig.canvas.get_renderer()).height/fig.get_window_extent().height*(1+pad)
         fig.subplots_adjust(top=1-w)
         cax = fig.add_axes([0, 1-w, 1, 1])
         loc = 'lower left'
